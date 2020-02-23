5e83d1aec7d6edf5a40b0088287d4eb9346b51e5
Shigeru Hagiwara <hgwrsgr@gmail.com>
2020-02-23 12:43:14 +0900

    writing tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,8 @@
 require "bundler/setup"
 require "finalizable"
-require "finalizable/example/parent_class"
-require "finalizable/example/a_class_allowed_to_override"
-require "finalizable/example/a_class_some_methods_are_not_allowed_overriding"
+require "lib/finalizable/example/parent_class"
+require "lib/finalizable/example/a_class_allowed_to_override"
+require "lib/finalizable/example/a_class_some_methods_are_not_allowed_overriding"
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
