85a99bacfc179590b07d125184ba2d5c86cca39f
Shigeru Hagiwara <hgwrsgr@gmail.com>
2020-02-23 12:05:20 +0900

    writing tests

---
+++ spec/finalizable/example/a_class_some_methods_are_not_allowed_overriding.rb
@@ -0,0 +1,24 @@
+module Finalizable
+  module Example
+    # This class is allowed overriding.
+    class AClassSomeMethodsAreNotAllowedOverriding < ParentClass
+      # final :first_method_defined_in_parent_class, :second_method_defined_in_parent_class
+
+      def first_method_defined_in_parent_class
+        :return_value_from_subclass
+      end
+
+      def second_method_defined_in_parent_class
+        :return_value_from_subclass
+      end
+
+      def third_method_defined_in_parent_class
+        :return_value_from_subclass
+      end
+
+      def a_method_defined_in_sub_class
+        :return_value_from_subclass
+      end
+    end
+  end
+end
